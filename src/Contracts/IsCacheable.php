65575bfa02521a22453f09f9a152b4e940c072a8
Arno <arno.premiums@gmail.com>
2017-02-25 17:01:44 +0100

    .

--- src/Contracts/IsCacheable.php
+++ src/Contracts/IsCacheable.php
@@ -8,6 +8,8 @@ namespace Qintuap\Scopes\Contracts;
  */
 interface IsCacheable {
 
+    // make a cache key for a method
     function makeCacheKey($method, $parameters);
+    // make cache tags for a method
     function makeCacheTags($method, $parameters);
 }
